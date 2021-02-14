f1414bc2ea04668fb0486dc1d098d49ca8f1f8eb
minnyww <minrrverse@gmail.com>
2021-02-14 11:50:09 +0700

    [init]

--- src/config/config.js
+++ src/config/config.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 firebase.initializeApp({
-   apiKey: "AIzaSyBg0LK0eKK8rorYdvL8vw5cgmoV3hUZmeg",
-   authDomain: "chayen.firebaseapp.com",
-   databaseURL: "https://chayen.firebaseio.com",
-   projectId: "chayen",
-   storageBucket: "chayen.appspot.com",
-   messagingSenderId: "562614183093",
-   appId: "1:562614183093:web:1ac671d1000f49b9ee4afc",
-   measurementId: "G-DJKHWJEEM6",
+   apiKey: "",
+   authDomain: "",
+   databaseURL: "",
+   projectId: "",
+   storageBucket: "",
+   messagingSenderId: "",
+   appId: "",
+   measurementId: "",
 });
 
 export const firebaseApp = firebase;
